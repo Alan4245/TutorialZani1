986ea877b30d6078e0b4751702aefa76afc15ce4
Alan4245 <alanminecraft77@gmail.com>
2020-11-17 14:23:10 +0100

    Aggiunta Classe Binaria

---
+++ ZaniDisnef/Binaria.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZaniDisnef
+{
+    class Binaria
+    {
+
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+        throw new Exception("Ti piacerebbe");
+
+    }
+}
